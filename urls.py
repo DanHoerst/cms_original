153d07f6f0db41d175c7aec1bd58eba6155b0998
Danny830x <dhoerst1@gmail.com>
2012-01-28 12:51:23 -0500

    first commit

---
+++ urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+from django.contrib.flatpages import urls
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'cms.views.home', name='home'),
+    # url(r'^cms/', include('cms.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    
+    url(r'^tiny_mce(?P<path>.*)$', 'django.views.static.serve', { 'document_root': '/home/dan/Downloads/tinymce/jscripts/tiny_mce' }),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^search/$', 'cms.search.views.search'),
+    url(r'', include(urls.urlpatterns)),
+)
